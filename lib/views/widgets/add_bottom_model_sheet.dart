02fe4afbbe99f56a144ab98e1dcb8d4d51ee3dc4
YoussefM50 <ym1557@fayoum.edu.eg>
2023-07-27 15:08:22 +0300

    create custom text field

--- lib/views/widgets/add_bottom_model_sheet.dart
+++ lib/views/widgets/add_bottom_model_sheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_hive/views/widgets/custom_text_fielsd.dart';
 
 class AddBottomModelSheet extends StatelessWidget {
   const AddBottomModelSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField()
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_text_fielsd.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_hive/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+          hintText: 'Title',
+          hintStyle: const TextStyle(color: kPrimaryColor),
+          border: buildborder(),
+          enabledBorder: buildborder(),
+          focusedBorder: buildborder(kPrimaryColor)),
+    );
+  }
+
+  OutlineInputBorder buildborder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: color ?? Colors.white,
+        ));
+  }
+}
