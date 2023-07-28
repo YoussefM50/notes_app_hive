fdc1b930c59ab81322199bca2c623e270185a9ca
YoussefM50 <ym1557@fayoum.edu.eg>
2023-07-28 17:59:11 +0300

    creat notes cubit

--- lib/Features/presentation/manager/cubits/notes_cubit/notes_cubit.dart
+++ lib/Features/presentation/manager/cubits/notes_cubit/notes_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app_hive/Features/data/models/notes_model.dart';
 import 'package:notes_app_hive/Features/presentation/manager/cubits/notes_cubit/notes_states.dart';
 
-class AddNoteCubit extends Cubit<NoteStates> {
-  AddNoteCubit() : super(NoteInitialState());
+import '../../../../../constants.dart';
 
+class NoteCubit extends Cubit<NoteStates> {
+  NoteCubit() : super(NoteInitialState());
+
+  fetchAllNotes() {
+    emit(NoteLoadingState());
+    try {
+      var notesbox = Hive.box<NotesModel>(kNoteBox);
+      List<NotesModel> notes = notesbox.values.toList();
+      emit(NoteSuccessState(notes));
+    } catch (e) {
+      emit(NoteErrorState(e.toString()));
+    }
+  }
 }
--- lib/Features/presentation/views/notes_view.dart
+++ lib/Features/presentation/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app_hive/Features/presentation/manager/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app_hive/Features/presentation/views/widgets/add_bottom_model_sheet.dart';
 import 'package:notes_app_hive/views/widgets/notes_view_body.dart';
 
@@ -7,21 +9,24 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)),
-              context: context,
-              builder: (context) {
-                return const AddBottomModelSheet();
-              });
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                context: context,
+                builder: (context) {
+                  return const AddBottomModelSheet();
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
